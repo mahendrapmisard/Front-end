c4e3a856e4947d190dcb2ffb1ddad27df7ed9f00
mahi <mahendrareddy1202@gmail.com>
2026-09-07 18:04:58 +0530

    some changes in main

--- Angular/myAngularApp/src/app/app.ts
+++ Angular/myAngularApp/src/app/app.ts
@@ -10,51 +10,7 @@ import { NgStyle } from '@angular/common';
   styleUrl: './app.css'
 })
 export class App {
-  // protected readonly title = signal('myAngularApp');
-  para : String = " this is normal para from ts file using string interpolation"
-
-  textvalue : string = "this is text from string interpolation into textbox"
-
-  name:String = ""
-  fname : String = ""
-
-  max_length:number = 10
-min_length : number = 5 
-  nameplaceholder :string = "enter your name"
-methodbinding(){
-  return "this is function return";
-}
-
-submitclicked(eveobj:any){
-  // console.log(" the submit got clicked");
-  this.fname = this.name
-
-}
 
 
-headingcolour = "blue";
-
-pickthename(nameeve:any){
-  // console.log("anem is entered")
-this.name = nameeve.srcElement.value
-// console.log(nameeve.srcElement.value)
-}
-
-isliked :boolean = false
-
-likes :number = 0;
-
-heartliked(){
-
-  // this.isliked = !this.isliked
-  if(this.isliked){
-    this.isliked = false;
-    this.likes = this.likes-1;
-  }
-  else {
-  this.isliked = true;
-  this.likes = this.likes+1;
-}
-}
-
+  
 }
